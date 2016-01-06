c440379a556d59d16e898421019063f5e45e4064
Amie Hsieh <Amie@D-173-250-148-142.dhcp4.washington.edu>
2016-01-06 14:56:30 -0800

    Sets up a node module

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 'use strict';
 
 /* Your code goes here! */
+var readline = require('readline');
+var io = readline.createInterface({ //call the interface "io"
+  input: process.stdin, //input comes from the terminal ("standard in")
+  output: process.stdout //output goes to the terminal ("standard out")
+});
+io.question('Where do you want to go?', function(answer) {
+	console.log('You said "'+answer.toUpperCase()+'"!');
+	io.close(); //close the whole interface once completely done asking questions
+});
