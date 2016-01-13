315cc71b0e0f9ae0f8331939afde389af02da961
Amie Hsieh <Amie@D-173-250-156-246.dhcp4.washington.edu>
2016-01-12 16:12:24 -0800

    Fixed functions, added to ReadMe file

--- README.md
+++ README.md
@@ -5,24 +5,23 @@ This module is a simple-text-based game called Nork, completed as part of a [cou
 The below questions should be answered (in detail!) regarding your submission!
 
 ##### 1. Explain how you organized the _data_ and _behaviors_ of your program. Did you break up your work into different _objects_, _functions_, or _files_? Reflect on why you chose to organize it the way that you did. #####
-> I decided to organize my program by functions. I knew that the game would have 4 commands: GO, TAKE, USE, INVENTORY. So I created a recursive function first for the progam to keep asking what the user would like to do. Then I created methods for each of the four actions. 
+> I decided to organize my program by functions. I knew that the game would have 4 commands: GO, TAKE, USE, INVENTORY. So I created a recursive function first for the progam to keep asking what the user would like to do. Then I created methods for each of the four actions. I also created a process function that would call either of the 4 command's methods based on what the user typed in 
 
 
 ##### 2. Does your application demonstrate good *coupling*? Explain your answer either way. #####
-> Answer goes here!
+> Yes my application demonstrates good coupling because my code pulls from the json file and they work together for the game to work. 
 
 
 ##### 3. Would your application be easy to update in the future (e.g., to add more rooms, items, actions)? Why or why not? #####
-> Yes it would be pretty easy to update in the future, the rooms and items would have to follow the same format, and for creaing new actions, new methods need to be made. But I dont think it would be too complicated to adjust in the future
+> Yes it would be pretty easy to update in the future, the rooms and items would have to follow the same format, and for creaing new actions, new methods need to be made. But I dont think it would be too complicated to adjust in the future.
 
 
 ##### 4. Approximately how many hours did it take you to complete this assignment? #####
-> This was the first assignment working with Java Script, Git and the command line, so I had to spend a lot of time reseraching JavaScript syntax online. I think the assigment including research probably took me over 24 hours. I also had a bit of trouble commiting my files to Git Hub, I followed the instructions on the specs but I kept getting errors saying that it couldnt commit. I'm not sure what I am doing wrong, so I wasnt able to commit my code each step of the way.
-
+> This was the first assignment working with Java Script, Git and the command line, so I had to spend a lot of time reseraching JavaScript syntax online. I think the assigment including research probably took me over 24 hours. I also had a bit of trouble commiting my files to Git Hub, I followed the instructions on the specs but I kept getting errors saying that it couldnt commit. I'm not sure what I was doing wrong. Also I spent most of my sunday night working on the assignment so I didnt commit each step of the way. I just commited at the end when I was done working on the assignment that day. 
 
 ##### 5. Did you receive help from any other sources (classmates, etc)? If so, please list who (be specific!). #####
-> Nope!
+> Sally Li helped me with setting up git hub, downloading git, teaching me some git commands. Joel Ross helped me with structuring my code, by adivising me to create a recursive function and functions for each of the commands. 
 
 
 ##### 6. Did you encounter any problems in this assignment we should warn students about in the future? How can we make the assignment better? #####
-> I didnt' encounter any problems through out the assigments besides having to do a lot of research. 
+> I didnt' encounter any problems through out the assigments besides having to do a lot of research. I just had a lot of trouble throughout the entire assignment because I am still not comfortable with javascript, git and the command line and incorporating json files. 
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var Current ={
 var world = require('./lib/world.json'); //import world file
  Current.Room = world.rooms[0]; //Stores the room that we are in
  
-var room = world.rooms[0]; //Stores the room that we are in
+//var room = world.rooms[0]; //Stores the room that we are in
 
 var readline = require('readline');
 var io = readline.createInterface({ //call the interface "io"
@@ -21,25 +21,20 @@ console.log('Welcome to the game of Nork! \n');
 console.log(Current.Room.description); //prints out the current room description
 
 //Function that recusively asks the user of what they want to do. Takes in user input as a parameter
-var askQuestion = function(answer)
-{
+var askQuestion = function(answer) {
     Process(answer.toUpperCase());
-    if (null != Current)
-    {
+    if (null != Current) {
         io.question('What would you like to do?', askQuestion);
     }
-    if (null === Current)
-     {
-        console.log('Good Bye');
+    if (null === Current) {
     	io.close(); //close the whole interface once completely done asking questions   
     }
 }
- io.question('What would you like to do?', askQuestion);
+io.question('What would you like to do?', askQuestion);
 
 //Function that processes the answer from the four commands the user can input, if its not one of the valid commands
 //The game is over
-function Process(answer)
-{
+function Process(answer) {
     if(answer.substr(0,3) === 'GO ') {
         ProcessGo(answer.substr(3).toLowerCase());
     } else if(answer.substr(0,5) === 'TAKE ') {
@@ -55,56 +50,46 @@ function Process(answer)
 }
 
 //Function to process the command go. 
-function ProcessGo(data){
-    if (Current.Room.exits[data])
-    {
-        var NewRoomName = Current.Room.exits[data];
+function ProcessGo(direction){
+    if (Current.Room.exits[direction]) {
+        var NewRoomName = Current.Room.exits[direction];
         for(var i = 0; i < world.rooms.length; i++) {
             if(world.rooms[i].id === NewRoomName) {
-                Current.rooms = world.rooms[i];
-                console.log('\n' + 'You moved ' + data + ', now you are in the ' + Current.rooms.id + '.');
-                console.log(Current.rooms.description + '\n');
+                Current.room = world.rooms[i];
+                console.log('\n' + 'You moved ' + direction + ', now you are in the ' + Current.room.id + '.');
+                console.log(Current.room.description + '\n');
             }
         }
     }
-    else{
+    else {
         console.log("Not a valid direction");
     }
 }
 
 function ProcessTake(itemName){
-    var itemPickedup = false;
-    //for(var i = 0; i < Current.Room.items.length; i++) {    
-        //if (Current.Room.items[i] === itemName) {
-             if(Current.Room.items = itemName) {
-                 console.log('You picked up: ' + itemName);
-             }
-            //console.log('You picked up: ' + itemName);
-            Current.inventory.push(itemName);
-            itemPickedup = true;
-        //}
-   // }
-    if(itemPickedup === false) {
+    if(Current.Room.items.name === itemName) {
+        console.log('You picked up: ' + itemName);
+        Current.inventory.push(itemName);
+    } else {
         console.log('Could not find ' + itemName);
     }
 }
 
-function ProcessUse(data) {
-    for(var i = 0; i < Current.Room.items.length; i++) {
-        if(Current.room.description.includes(Current.room.items[i])) {
-            
-        }   
-    //Check inventory make sure item exists
-    //verify item can be used in the room (make sure word is in task,)
-    
-    // to end just set Current=null;
-    //if (Current.Room.exits[data])
+function ProcessUse(itemName) { 
+    var inInventory = false;
+    for(var i = 0; i < Current.inventory.length; i++) {
+        if(Current.inventory[i] === itemName) {
+            inInventory = true;
+        }
+    }
+    if (inInventory === true && Current.Room.description.includes(Current.Room.items.task)) {
+       console.log('You are now using the ' + itemName);
+    } else {
+            console.log('Sorry you dont have that item!');
     }
 }
 
 function ProcessInventory() {
     console.log('Current inventory: ');  
-    for(var i = 0; i < Current.inventory.length; i++) {
-         console.log('   ' +Current.inventory.id);  
-    }
+         console.log('   ' + Current.inventory);  
 }
